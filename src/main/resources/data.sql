ed3bd79756628fd8de1e962a66ef3069addbaa7b
Rhull11 <rhull9@outlook.com>
2025-03-27 08:56:01 -0600

    Removing test data source from project

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Item (id, asin, name, ingredients, version)
-VALUES
-(1, 'B082BNYRFD','Sriracha Hot Chili Sauce','water, jalepano pepper puree, sugar, cayenne pepper puree, garlic, soy sauce', 0);
