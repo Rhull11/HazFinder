712eeb5ce654239b1adfe884e65d69bd754d74b1
Rhull11 <rhull9@outlook.com>
2025-07-22 22:30:15 -0600

    Adding "thumbnail" column to schema

--- src/main/java/com/rhull/hazfinder/item/Item.java
+++ src/main/java/com/rhull/hazfinder/item/Item.java
@@ -1,11 +1,11 @@
 package com.rhull.hazfinder.item;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Version;
 
 public record Item(
         @Id
         Integer id,
+        String thumbnail,
         String asin,
         String name,
         String ingredients
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS Item (
     id INT GENERATED ALWAYS AS IDENTITY,
-    asin varchar(250) NOT NULL,
-    name varchar(250) NOT NULL,
-    ingredients varchar(250) NOT NULL,
+    thumbnail TEXT,
+    asin VARCHAR(250) NOT NULL,
+    name VARCHAR(250) NOT NULL,
+    ingredients TEXT NOT NULL,
     PRIMARY KEY (id)
 );
